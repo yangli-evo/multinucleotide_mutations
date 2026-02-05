efc69ead1f8117a8b5e4f7d764299e855dc720cd
Yang Li <yangli.evo@gmail.com>
2026-02-05 17:13:14 -0500

    Add imports and define __all__ in __init__.py

--- src/mnm_mutations/__init__.py
+++ src/mnm_mutations/__init__.py
@@ -1,1 +1,14 @@
+# src/mnm_mutations/__init__.py
+from .mutants import (
+    generate_adjacent_double_mutants,
+    generate_double_mutants,
+    generate_single_base_mutants,
+    validate_dna_sequence,
+)
 
+__all__ = [
+    "validate_dna_sequence",
+    "generate_single_base_mutants",
+    "generate_adjacent_double_mutants",
+    "generate_double_mutants",
+]
